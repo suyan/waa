cd57144386ff2bfa23af3a2862dfeea41bb95e64
SuYan <suyan0830@gmail.com>
2014-04-30 10:09:27 +0800

    minimize this file so that I can look it simply

--- app/commands/LorgCommand.php
+++ app/commands/LorgCommand.php
@@ -5,84 +5,49 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 
 class LorgCommand extends Command {
+    protected $name = 'lorg';
+    protected $description = 'Test lorg in command';
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function fire()
+    {
+        $this->info('开始分析');
+        // 以Laravel格式获得配置文件，配置文件在waa中
+        $lorg = new Suyan\Lorg\Lorg('Laravel', 'waa');
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'lorg';
+        // 输入方式，从文件输入
+        $lorg->config->set('input', array(
+            'type'=>'File', 
+            'File'=>app_path().'/libraries/Suyan/Lorg/Data/log_test'
+            // 'File' => '/Users/yansu/Downloads/access_log'
+            ));
+        
+        include_once '/usr/local/Cellar/php55-xhprof/254eb24/xhprof_lib/utils/xhprof_lib.php';
+        include_once '/usr/local/Cellar/php55-xhprof/254eb24/xhprof_lib/utils/xhprof_runs.php'; 
+        xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+         
+        $lorg->run();
+         
+        $xhprof_data = xhprof_disable();
+        $profiler_namespace="hello";
+        $xhprof_runs = new XHProfRuns_Default();
+        $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
+    }
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Test lorg in command';
+    protected function getArguments()
+    {
+        return array(
+            // array('example', InputArgument::REQUIRED, 'An example argument.'),
+        );
+    }
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
-		$this->info('开始分析');
-		$lorg = new Suyan\Lorg\Lorg('Laravel', 'waa');
-		// 从文件中读取配置项
-		$lorg->config->set('input', array(
-		    'default'=>'File', 
-		    'File'=>app_path().'/libraries/Suyan/Lorg/Data/log_test'
-		    // 'File' => '/Users/yansu/Downloads/access_log'
-		    ));
-		
-		include_once '/usr/local/Cellar/php55-xhprof/254eb24/xhprof_lib/utils/xhprof_lib.php';
-		include_once '/usr/local/Cellar/php55-xhprof/254eb24/xhprof_lib/utils/xhprof_runs.php';
-		 
-		xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-		 
-		$lorg->run();
-		 
-		$xhprof_data = xhprof_disable();
-		 
-		$profiler_namespace="hello";
-		$xhprof_runs = new XHProfRuns_Default();
-		$run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
-		
-	}
-
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return array(
-			// array('example', InputArgument::REQUIRED, 'An example argument.'),
-		);
-	}
-
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array(
-			// array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-		);
-	}
+    protected function getOptions()
+    {
+        return array(
+            // array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+        );
+    }
 
 }
