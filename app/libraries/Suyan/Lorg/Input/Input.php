26f940df99495922d18b549e43226a9d4645f08d
SuYan <suyan0830@gmail.com>
2014-04-30 10:40:30 +0800

    fix bug in input.php

--- app/libraries/Suyan/Lorg/Input/Input.php
+++ app/libraries/Suyan/Lorg/Input/Input.php
@@ -3,7 +3,7 @@
 * @Author: Su Yan <http://yansu.org>
 * @Date:   2014-03-22 18:35:57
 * @Last Modified by:   Su Yan
-* @Last Modified time: 2014-04-30 10:02:47
+* @Last Modified time: 2014-04-30 10:40:03
 */
 namespace Suyan\Lorg\Input;
 
@@ -17,16 +17,17 @@ class Input implements InputInterface
 
         // 检查输入类型
         if (!isset($opts['type'])) 
-            throw new Exception("invalid opts of input");        
+            throw new \Exception("invalid opts of input"); 
+        $type = $opts['type'];
         $input = 'Suyan\Lorg\Input\\'.$type.'Input';
 
         if (!class_exists($input)) 
-            throw new Exception("$input doesn't exists");
+            throw new \Exception("$input doesn't exists");
 
         // 实例化输入类型
-        if (!isset($opts[$opts['type']])) 
-            throw new Exception("invalid opts of input");
-        $this->input = new $input($opts[$opts['type']]);
+        if (!isset($opts[$type])) 
+            throw new \Exception("invalid opts of input");
+        $this->input = new $input($opts[$type]);
     }
 
     public function resetInput(){
